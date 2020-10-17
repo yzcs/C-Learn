708265e98b10946cf4eb947cf62e6cca08b6c758
yzcs <mxz0911@gmail.com>
2020-10-17 00:13:20 -0400

    Modify my Calss: CAR

--- CSharpLearn1/Car.cs
+++ CSharpLearn1/Car.cs
@@ -5,14 +5,18 @@ namespace CSharpLearn1
     class Car
     {
         public string color = "red";
-        /*
+        int maxSpeed;
+
+        
         static void Main(string[] args)
         {
-            Car myObj1 = new Car();
-            Car myObj2 = new Car();
-            Console.WriteLine(myObj1.color);
-            Console.WriteLine(myObj2.color);
-        }*/
+            Car myObj = new Car();
+            //Car myObj2 = new Car();
+            myObj.maxSpeed = 200;
+            Console.WriteLine(myObj.color);
+            //Console.WriteLine(myObj2.color);
+            Console.WriteLine(myObj.maxSpeed);
+        }
     }
 
 
