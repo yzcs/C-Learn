8673f87c462a4ef2c96f08fb5a2d34e71e0d9280
yzcs <mxz0911@gmail.com>
2020-10-17 00:05:32 -0400

    New Updates

---
+++ CSharpLearn1/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace CSharpLearn1
+{
+    class Program
+    {
+        public static void Main(String []args)
+        {
+            Car myObj1 = new Car();
+            Console.WriteLine(myObj1.color);
+        }
+    }
+}
